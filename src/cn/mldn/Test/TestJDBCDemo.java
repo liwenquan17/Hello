18c07caad300ced7f5a4153ea1b72ecb2b3b0af7
liwenquan <liwenquan@liwenquan-PC>
2017-04-27 22:08:19 +0800

    first

---
+++ src/cn/mldn/Test/TestJDBCDemo.java
@@ -0,0 +1,17 @@
+package cn.mldn.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJDBCDemo {
+	public static final String DBDRIVER = "oracle.jdbc.driver.OracleDriver" ;
+	public static final String DBURL = "jdbc:oracle:thin:@localhost:1521:mldn" ;
+	public static final String DBUSER = "scott" ;
+	public static final String DBPASSWORD = "tiger" ;
+	public static void main(String[] args) throws Exception {
+		Class.forName(DBDRIVER) ;	// 进行数据库驱动的加载
+		Connection conn = DriverManager.getConnection(DBURL, DBUSER, DBPASSWORD) ;
+		System.out.println(conn);
+		conn.close(); 
+	}
+}
